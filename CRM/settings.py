c768f3e42ef33270556a8f40ce30a421309d7aa0
Fashaan <67037844+fashan7@users.noreply.github.com>
2023-07-18 15:14:27 +0200

    completed the new page insertion, if nor records

--- CRM/settings.py
+++ CRM/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'accounts',
     'leads',
     'pagesallocation',
+    'clearcache',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- CRM/urls.py
+++ CRM/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
     path('', include('accounts.urls')),
