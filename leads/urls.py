231dc3efca5fbfafd78cc0bdeb1102300f7700e7
Fashaan <67037844+fashan7@users.noreply.github.com>
2023-07-26 20:44:11 +0200

    feat: bulk assign for a user the leads

--- leads/urls.py
+++ leads/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('import_leads/', views.import_leads, name='import_leads'),
     path('export_leads/<str:file_format>/', views.export_leads, name='export_leads'),
     path('sales_lead/',views.sales_lead,name="sales_lead"),
+    path('assign_leads/', views.assign_leads, name='assign_leads'),
 ]
 
 
