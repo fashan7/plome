c1d0b8d3d283419377849b00d38c5ac758eb2f96
Jatin Kant <verma.jatin6661@gmail.com>
2023-08-18 13:20:40 +0200

    update: reminder.py

--- leads/tasks/reminder.py
+++ leads/tasks/reminder.py
@@ -7,17 +7,26 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.utils import timezone
 import logging  
+from django.db.models import F
 
 error_logger = logging.getLogger('error_logger')
 
 @shared_task(name="send_appointment_reminder")
 def send_appointment_reminder():
     try:
+        
+        #here right? yes
         leads = Lead.objects.filter(
-            appointment_date_time__minute=timezone.now().minute - 15,
+            appointment_date_time__minute= F('appointment_date_time__minute') - 2,
             read_mail=False,
         )
-
+        leads_ = Lead.objects.all()
+        for l in leads_:
+            print(l.appointment_date_time," -- ",l.appointment_date_time__minute, " ****  ", l.appointment_date_time__minute+2)
+        
+        print("****************************")
+        print("Real Var") #yes are we running redis? doesnt the file doesnt matter 
+        print(leads)
         for lead in leads:
             subject = "Reminder: Appointment Coming Up"
             message = f"Your appointment is coming up in 15 minutes at {lead.appointment_date_time}."
@@ -26,6 +35,8 @@ def send_appointment_reminder():
             )
             lead.read_mail = True
             lead.save(update_fields=["read_mail"])
-    except Exception as e:
-        error_logger.info(e)
-    return True
+    except:
+        print('Shit Exception')
+        #error_logger.info(e) #check this log
+        
+    return True #restart e
